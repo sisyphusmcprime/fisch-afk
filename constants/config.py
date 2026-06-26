2962bae25fb8b63996d1348e4b9d37b8c7daee32
The Trolling Wizard <luckyozonejimmi@gmail.com>
2026-06-26 15:55:05 -0700

    Added color.json and reordered files

--- constants/config.py
+++ constants/config.py
@@ -1,16 +1,24 @@
 """
 
-    -- config.py --
+-- config.py --
 
-    Imports all the stored json data into Python.
+Imports all the stored json data into Python.
 
 """
+
 import json
 
+
 def init_pos(run_path):
-    with open(f'{run_path}/json/position.jsonc', 'r') as pos_json:
+    with open(f"{run_path}/constants/json/position.jsonc", "r") as pos_json:
         global pos_consts
         pos_consts = json.load(pos_json)
 
+def init_color(run_path):
+    with open(f"{run_path}/constants/json/colors.jsonc") as color_json:
+        global color_consts
+        color_consts = json.load(color_json)
+
 def init(run_path):
     init_pos(run_path)
+    init_color(run_path)
