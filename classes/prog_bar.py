4d030a90f2aa9b6044662b1575828d50116e0165
The Trolling Wizard <luckyozonejimmi@gmail.com>
2026-06-30 17:59:03 -0700

    Added __init__() to ProgressBar() in prog_bar.py

---
+++ classes/prog_bar.py
@@ -0,0 +1,22 @@
+"""
+-- lure_bar.py --
+
+Class for the lure bar in the fishing minigame.
+Includes code for handling the lure bar and reeling in fish.
+
+"""
+
+import constants.config as cfg
+from classes.window import Window
+
+import time
+
+
+class ProgressBar(Window):
+    def __init__(self):
+        self.prog_bar_props = cfg.pos_consts["prog_bar"]
+
+        self.prog_bar_pos = {"x": self.prog_bar_props["pos"][0], "y": self.prog_bar_props["pos"][1]}
+        self.prog_bar_scale = {"width": self.prog_bar_props["scale"][0], "height": self.prog_bar_props["scale"][1]}
+
+        self.prog_bar_color = cfg.color_consts["prog_bar"]
