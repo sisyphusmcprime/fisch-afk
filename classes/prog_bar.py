fdd48d031b2ff66cccf51e8cdb078ebd76bdfa63
The Trolling Wizard <luckyozonejimmi@gmail.com>
2026-06-30 18:03:23 -0700

    Added are_we_there_yet() function to ProgressBar() in prog_bar.py and reformatted the file using Black

--- classes/prog_bar.py
+++ classes/prog_bar.py
@@ -16,7 +16,22 @@ class ProgressBar(Window):
     def __init__(self):
         self.prog_bar_props = cfg.pos_consts["prog_bar"]
 
-        self.prog_bar_pos = {"x": self.prog_bar_props["pos"][0], "y": self.prog_bar_props["pos"][1]}
-        self.prog_bar_scale = {"width": self.prog_bar_props["scale"][0], "height": self.prog_bar_props["scale"][1]}
+        self.prog_bar_pos = {
+            "x": self.prog_bar_props["pos"][0],
+            "y": self.prog_bar_props["pos"][1],
+        }
+        self.prog_bar_scale = {
+            "width": self.prog_bar_props["scale"][0],
+            "height": self.prog_bar_props["scale"][1],
+        }
 
         self.prog_bar_color = cfg.color_consts["prog_bar"]
+
+    def are_we_there_yet(self):
+        return self.color_at_pixel(
+            self.prog_bar_color,
+            (
+                self.prog_bar_pos["x"] + (self.prog_bar_scale["width"] / 2),
+                self.prog_bar_pos["y"],
+            ),
+        )
